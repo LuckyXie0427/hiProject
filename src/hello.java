c20783ee210245cf0d94cc0d5cba237aad83576d
xwq <1581099023@qq.com>
2023-02-28 18:21:46 +0800

    init

---
+++ src/hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: xwq980427@163.com
+ * @Date: 2023/2/28 18:20
+ * Description:
+ * Usage:
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello~");
+    }
+}
